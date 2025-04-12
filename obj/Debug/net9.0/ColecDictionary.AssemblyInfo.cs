8c5ee3a5d83be4ba98c916e1da919d5631e25fa9
Miguel Zamora <miguelangelzt129@gmail.com>
2025-04-11 22:11:07 -0600

    Actualizando salida

--- obj/Debug/net9.0/ColecDictionary.AssemblyInfo.cs
+++ obj/Debug/net9.0/ColecDictionary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ColecDictionary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4cfc279d5d8be50009b790dc14f2def189e7e39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654328fe27118ae6884056323c8cc497f2066ce6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ColecDictionary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ColecDictionary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
